adf5c13c16d7aa84fd3f4e67f7b47bb186db52e7
Rob Hess <hessro@oregonstate.edu>
2023-11-27 14:49:21 -0800

    Add code from 11/27 lecture.

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 var express = require("express")
 var app = express()
 
+var logger = require("./lib/logger")
+
+app.use(logger)
+
+app.use(express.static("static"))
+
 app.get("/about", function (req, res, next) {
-    console.log("== GET request for /about")
+    // console.log("== GET request for /about")
 
     // res.type("text/html")
     // res.status(200)
@@ -12,25 +18,36 @@ app.get("/about", function (req, res, next) {
 })
 
 app.get("/", function (req, res, next) {
-    console.log("== GET request for /")
+    // console.log("== GET request for /")
     res.status(200).sendFile(__dirname + "/static/index.html")
 })
 
 app.get("/people", function (req, res, next) {
-    console.log("== GET request for /people")
+    // console.log("== GET request for /people")
     res.status(200).sendFile(__dirname + "/static/people.html")
 })
 
+var availablePeople = [
+    "luke",
+    "leia",
+    "rey",
+    "finn",
+    "r2d2"
+]
+
 app.get("/people/:person", function (req, res, next) {
-    console.log("== GET request for /people/:person")
+    // console.log("== GET request for /people/:person")
     console.log("  -- req.params:", req.params)
     var person = req.params.person
-    res.status(200).sendFile(__dirname + "/static/people/" + person + ".html")
-    // next()
+    if (availablePeople.indexOf(person) !== -1) {
+        res.status(200).sendFile(__dirname + "/static/people/" + person + ".html")
+    } else {
+        next()
+    }
 })
 
 app.get("*", function (req, res, next) {
-    console.log("== 404!")
+    // console.log("== 404!")
     res.status(404).sendFile(__dirname + "/static/404.html")
 })
 
